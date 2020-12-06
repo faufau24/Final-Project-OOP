0aea4d9412af12ddc06b09b12c103ab2d6d7e31a
aidilfitra08 <aidil.gitu1@gmail.com>
2020-12-06 17:19:03 +0700

    Update Sudoku.java

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,13 +1,12 @@
-import javax.lang.model.util.ElementScanner6;
-
 public class Sudoku {
     public static void main(String[] args) {
+        // SudokuApp s1 = new SudokuApp("EASY", 4);
         if (args[0].equals("easy")) {
-            SudokuBoard sudokuBoard = new SudokuBoard("EASY", 4);
+            SudokuApp sudokuApp = new SudokuApp("EASY", 4);
         } else if (args[0].equals("medium")) {
-            SudokuBoard sudokuBoard = new SudokuBoard("MEDIUM", 2);
+            SudokuApp sudokuApp = new SudokuApp("MEDIUM", 2);
         } else if (args[0].equals("hard")) {
-            SudokuBoard sudokuBoard = new SudokuBoard("HARD", 1);
+            SudokuApp sudokuApp = new SudokuApp("HARD", 1);
         }
     }
 }
